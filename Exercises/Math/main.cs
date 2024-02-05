4cfa641af0d97ea9e1dbb32147b02467fdca7f04
SofieStougaard <sofie.kamp.stougaard@gmail.com>
2024-02-05 11:36:14 +0100

    _

---
+++ Exercises/Math/main.cs
@@ -0,0 +1,20 @@
+using System;
+using static System.Console;
+static class main{
+	static double x=1.0;
+	static int i;
+	static string hello = $"Hello, x={x}\n";
+	static double times2(double y){
+		double x=7;
+		{
+		double a=9;
+		}
+		double z=7;
+		WriteLine(x);
+		return y*2;
+	}
+	static int Main(){
+		Write($"x={x} Sin(x)={System.Math.Sin(x)}\n");
+	return 0;
+	}
+}
