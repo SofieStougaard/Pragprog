b4c3d469887c5b1ee4af9c26a896dc851360005c
SofieStougaard <sofie.kamp.stougaard@gmail.com>
2024-02-07 10:07:21 +0100

    Math exercise

--- Exercises/Math/main.cs
+++ Exercises/Math/main.cs
@@ -6,7 +6,7 @@ static class main{
 	static double y=2.0;
 	static double PI = 3.1415927;
 	static double e = 2.718281828459;
-	static string hello = $"hello\n";
+	//static string hello = $"hello\n";
 	static double Sin(double x){ 
 		return System.Math.Sin(x); 
 	}
@@ -23,19 +23,24 @@ static class main{
 		return System.Math.Exp(x);
 	}
 	static int Main(){
-		Write($"{hello}");
+		Write($"Part 1\n");
 		Write($"x={x}, x*2={times2(x)}\n");
 		Write($"x={x}, Sin(x)={Sin(x)}\n");
 		Write($"x={y}, sqrt(x)={Sqrt(y)}, sqrt(x)^2={Sqrt(y)*Sqrt(y)}\n");
 		Write($"x={y}, x^(1/5)={Power(2, 0.2)}, x^(1/5)^5={Power(Power(2,0.2), 5)}\n");
 		Write($"x=e, e^PI={Exp(PI)}, another way e^PI={Power(e,PI)}\n");
 		Write($"x=PI, PI^e={Power(PI,e)}\n");
+		Write($"\nPart 2\n");
 		double prod=1;
 		for(int x=1;x<10;x+=1){
 			Write($"fgamma({x})={sfuns.fgamma(x)} {x-1}!={prod}\n");
 			prod*=x;
+		}
+		Write($"\nPart 3\n");
+		for(int x=1;x<10;x+=1){
 			Write($"lngamma({x})={sfuns.lnfgamma(x)} \n");
-			}
+			prod*=x;
+		}
 	return 0;
 	}
 }
