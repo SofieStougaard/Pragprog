cb3ad2594b76403250b5ff6aa3bef50be101d8b8
SofieStougaard <sofie.kamp.stougaard@gmail.com>
2024-02-14 11:32:42 +0100

    vec

--- Exercises/Vec/main.cs
+++ Exercises/Vec/main.cs
@@ -1,13 +1,11 @@
 using System;
 using static System.Console;
+using System.Collections.Generic;
 using static vec;
 //methods:
 static class main{
         static int Main(){
-                //public void print(string s){Write(s);
-                //      WriteLine($"{x} {y} {z}");}
-                //public void print(){this.print("");}
-		vec a = new vec(2, 0, 0);
+	/*	vec a = new vec(2, 0, 0);
 		vec b = new vec(0, 1, 0);
 		Write($"ax={a.x} ay={a.y} az={a.z}\n");
 		Write($"bx={b.x} by={b.y} bz={b.z}\n");
@@ -15,7 +13,30 @@ static class main{
 		bool c = a.approx(a);
 		Write($"{c1} {c}\n");
 		vec c2 = cross(a,b);
-		Write($"{c2.x} {c2.y} {c2.z}\n");
+		Write($"{c2.x} {c2.y} {c2.z}\n"); */
+		List<vec> randomVectors = GenerateRandomVectors(5); // Change 5 to the desired number of vectors
+
+        	foreach (var vector in randomVectors)
+        	{
+			Console.WriteLine($"x={vector.x} y={vector.y} z={vector.z}");
+        	}
 		return 0;
-}//Main
+	}
+
+		static List<vec> GenerateRandomVectors(int numberOfVectors)
+    		{
+        		List<vec> vectors = new List<vec>();
+        		Random random = new Random();
+
+        		for (int i = 0; i < numberOfVectors; i++)
+        		{
+            			double x = random.NextDouble()*10-5;
+            			double y = random.NextDouble()*10-5;
+            			double z = random.NextDouble()*10-5;
+
+            			vectors.Add(new vec(x, y, z));
+        		}
+
+        	return vectors;
+		}
 }//main
