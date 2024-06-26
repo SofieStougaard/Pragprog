c214c4c8460ead731c096f3eb79d9ac2d37ce3ce
SofieStougaard <sofie.kamp.stougaard@gmail.com>
2024-06-26 12:21:56 +0200

    updates

--- Homework/Monte_Carlo/main.cs
+++ Homework/Monte_Carlo/main.cs
@@ -32,9 +32,9 @@ public class MonteCarlo{
 		return result;
 	}//plainMC
 
-	static double corput(int n, int b){
+	public static double corput(int n, int b){
 		double q = 0;
-		double bk = 1/b;
+		double bk = (double)1/b;
 		while (n > 0){
 			q += (n % b)*bk;
 			n /= b;
@@ -43,7 +43,7 @@ public class MonteCarlo{
 		return q;
 	}//corput 
 
-	static vector Halton(int n, int b){
+	public static vector Halton(int n, int b){
 		vector x = new vector(b);
 		int[] Base = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67 };
 		int b_max = Base.Length;
@@ -61,70 +61,69 @@ public class MonteCarlo{
                         V *= b[i] - a[i];
                 }
 
-                double sum = 0;
-                double sum2 = 0;
-                var x = new vector(dim);
-
-                for (int i = 0; i<N; i++){
+                double sum_c = 0;
+                double sum_h = 0;
+                var x_c = new vector(dim);
+		var x_h = new vector(dim);
+		
+                for (int i = 1; i<N; i++){
 			var halt = Halton(i, dim);
-			var corp = corput(i, 19);
-                        for (int k = 0; k < dim; k++){
-                                x[k] = a[k] + halt[k]*(b[k] - a[k]);
+			for (int k = 0; k < dim; k++){
+                                x_h[k] = a[k] + halt[k]*(b[k] - a[k]);
                         }
-                        sum += f(x);
+                        sum_h += f(x_h);
+			
                         for (int k = 0; k < dim; k++){
-				x[k] = a[k] + corp*(b[k] - a[k]);
+				x_c[k] = a[k] + corput(i+k*N, 19)*(b[k] - a[k]);
 			}
-			sum += f(x);
+			sum_c += f(x_c);
                 }
 
-                double mean = sum/N;
-                double sigma = Abs(sum - sum2)/N;
-
+                double mean = sum_h/N;
+                double sigma = Abs(sum_h - sum_c)/N;
                 var result = (mean*V, sigma*V); //integrale-værdi, estimeret fejl
+
                 return result;
-        }//HaltonMC
+        }//QuasiMC
 }//MonteCarlo
 
 public class main{
 	public static void Main(){
-		Func<vector, double> UnitCircle = (vector x) => {
-			return (x[0]*x[0] + x[1]*x[1] <= 1) ? 1 : 0;
-		};
+		Func<vector, double> UnitCircle = z => z[0];
 
-		vector a = new vector(-1, -1);
-		vector b = new vector(1, 1);
+		vector a = new vector(0, 0);
+		vector b = new vector(1, 2*PI);
+
+		Func<vector, double> CosIntegral =  z => 1.0 / ((1.0 - Cos(z[0]) * Cos(z[1]) * Cos(z[2])) * PI * PI * PI);
+
+                vector acos = new vector(0, 0, 0);
+                vector bcos = new vector(PI, PI, PI);
 		
-		string data = "UnitCircle.txt";
-		using (StreamWriter writer = new StreamWriter(data)){
-		for (int N=10; N < 500000; N+=1000){
+		string plainUnit = "UnitCircle.txt";
+		using (StreamWriter writer = new StreamWriter(plainUnit)){
+		for (int N=1; N < 10000; N+=200){
 			var result = MonteCarlo.plainMC(UnitCircle, a, b, N);
 			writer.WriteLine($"{N} {result.Item1} {result.Item2} {1/Sqrt(N)}");
 		}}
 
-                string dataquasi = "UnitCirclequasi.txt";
-                using (StreamWriter writer = new StreamWriter(dataquasi)){
-                for (int N=10; N < 500000; N+=1000){
+                string quasiunit = "UnitCirclequasi.txt";
+                using (StreamWriter writer = new StreamWriter(quasiunit)){
+                for (int N=1; N < 10000; N+=200){
                         var result = MonteCarlo.quasiMC(UnitCircle, a, b, N);
                         writer.WriteLine($"{N} {result.Item1} {result.Item2}");
                 }}
 		
-
-		Func<vector, double> CosIntegral = (vector x) => {
-			double cosprod = Cos(x[0]) * Cos(x[1]) * Cos(x[2]);
-			return 1.0 / (1 - cosprod);
-		};
-
-		vector acos = new vector(0, 0, 0);
-		vector bcos = new vector(PI, PI, PI);
-
-		int Ncos = 500000;
-		var resultcos = MonteCarlo.plainMC(CosIntegral, acos, bcos, Ncos);
-		var Haltoncos = MonteCarlo.quasiMC(CosIntegral, acos, bcos, Ncos);
+		int Ncos = 100000;
+		var plaincos = MonteCarlo.plainMC(CosIntegral, acos, bcos, Ncos);
+		var plainunit = MonteCarlo.plainMC(UnitCircle, a, b, Ncos);
 		
-		Console.WriteLine($"Plain Monte Carlo: Estimated value = {resultcos.Item1/Pow(PI,3)}, Estimated error = {resultcos.Item2}");
-		Console.WriteLine($"Halton Monte Carlo: Estimated value = {Haltoncos.Item1/Pow(PI,3)}, Estimated error = {Haltoncos.Item2}");
-		
-		Console.WriteLine($"Actual value = {1.3932039296856768591842462603255}");
+		var Quasicos = MonteCarlo.quasiMC(CosIntegral, acos, bcos, Ncos);
+		var Quasiunit =  MonteCarlo.quasiMC(UnitCircle, a, b, Ncos);
+
+		Console.WriteLine($"Plain Monte Carlo: Estimated value = {plainunit.Item1}, Estimated error = {plainunit.Item2} actual value {PI}");
+		Console.WriteLine($"Plain Monte Carlo: Estimated value = {plaincos.Item1}, Estimated error = {plaincos.Item2} actual value 1.3932039");
+
+		Console.WriteLine($"Quasi Monte Carlo: Estimated value = {Quasiunit.Item1}, Estimated error = {Quasiunit.Item2} actual value {PI}");
+		Console.WriteLine($"Quasi Monte Carlo: Estimated value = {Quasicos.Item1}, Estimated error = {Quasicos.Item2} actual value 1.3932039");
 	}//Main
 }//main
